529c1f0f8d610dd3cf50525b1326360dc0977b2a
Seth Harper <sharperprogrammer@gmail.com>
2019-06-07 19:46:41 -0400

    Added the last couple of pages to the site. Updated the text to be somewhat more professional.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,10 @@ class PagesController < ApplicationController
   def about
   end
 
+  def projects
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'projects', to: 'pages#projects'
+  get 'contact', to: 'pages#contact'
 end
